333f56e24e7fef53a62d2c5a3be503155995ac89
bhgou <108895891+bhgou@users.noreply.github.com>
2023-11-19 00:25:34 +0300

    hdrp

---
+++ Assets/Go Systems/GoCharacterController/script/AudioSpeaker.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace GoSystem
+{
+    [GBehaviourAttributeAttribute("Audio Speaker", false)]
+    public class AudioSpeaker : GoSystemsBehaviour
+    {
+        public AudioClip[] Clip;
+        bool lockInput;
+        void Play(AudioClip audioSource)
+        {
+         
+            gameObject.GetComponent<AudioSource>().clip = audioSource;
+
+            gameObject.GetComponent<AudioSource>().Play();
+        }
+        public void Stop()
+        {
+            gameObject.GetComponent<AudioSource>().Stop();
+           
+        }
+
+        public void IndexClip(int Index )
+        {
+                Play(Clip[Index]);
+        }
+
+        public void ApplyLoop(bool Loop)
+        {
+            gameObject.GetComponent<AudioSource>().loop = Loop;
+        }
+
+    }
+  
+}
